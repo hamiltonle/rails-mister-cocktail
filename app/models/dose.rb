91047defc6bd467994b923f48d7365b917494bea
Hamilton Le <hamilton.m.le@gmail.com>
2017-06-15 12:45:31 +0800

    created all models with validations

---
+++ app/models/dose.rb
@@ -0,0 +1,9 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail
+
+  validates :description, presence: true
+  validates :cocktail, presence: true
+  validates :ingredient, presence: true
+  validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+end
